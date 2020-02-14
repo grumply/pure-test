b666959c5f6585ab216477c1bdfb355e5eca03bb
Sean Hickman <sean@grumply.com>
2020-02-13 23:36:48 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, deepseq, directory, unordered-containers, hashable, mtl, stm, async, pure-random-pcg, pure-spacetime, pure-txt, pure-lifted, stdenv }:
 mkDerivation {
   pname = "pure-test";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base deepseq directory unordered-containers hashable mtl stm async pure-random-pcg pure-spacetime pure-txt pure-lifted ];
   homepage = "github.com/grumply/pure-test";
